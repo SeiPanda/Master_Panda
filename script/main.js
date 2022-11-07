1c036a1a8460909ae15a96dbadd3b30d1ffde824
Michelle Sandner <michelle.sandner@springernature.com>
2022-11-07 15:32:35 +0100

    added versus game mode

---
+++ script/main.js
@@ -0,0 +1,193 @@
+
+let currentCodeArray = ["", "", "", ""];
+let currentColorPalette = [];
+let rowCount = 7;
+let colorRows = [];
+let currentField = 0;
+let gameMode;
+let default_color_field_fa_circle = document.querySelectorAll(".default_color_field > .fas.fa-circle");
+let default_color_field = document.querySelectorAll(".default_color_field");
+
+document.querySelector("#heading-container").addEventListener("click", handleHeadingClick);
+
+default_color_field.forEach(color_field => {
+    color_field.addEventListener("click", handleColorClick);
+})
+
+document.querySelector("#reload_button").addEventListener("click", handleReloadButton);
+
+GetData();
+
+StartGame();
+
+function handleHeadingClick(){
+    window.location="menu.html";
+}
+
+function GetData(){
+    currentColorPalette = JSON.parse(localStorage.getItem("defaultColorPalette"));
+    gameMode = JSON.parse(sessionStorage.getItem("gameMode"));   
+    if(gameMode === "Versus"){
+        document.querySelector("#reload_button").style.display = "none";
+        document.querySelector("#placeholder").style.display = "none";
+    } 
+}
+
+
+function handleReloadButton() {
+    RestartGame();
+}
+
+function StartGame(){
+    setColoring();
+}
+
+let guessCode_fa_circle =  document.querySelectorAll(".guessCode_color_field > .fas.fa-circle");
+let result_field_fa_circle =  document.querySelectorAll(".result_field > .fas.fa-circle");
+let color_row_fa_circle = document.querySelectorAll(".colorRow > .color_field > .fas.fa-circle");
+let mask = document.querySelector(".mask");
+
+function RestartGame(){
+    currentField = 0;
+    colorRows = [];
+    rowCount = 7;
+    enableDefaultColorButtons();
+
+   color_row_fa_circle.forEach(item => {
+        item.style.color = "rgba(255, 255, 255, 0.363)"; 
+    });
+
+   result_field_fa_circle.forEach(item => {
+        item.classList.remove("rightColor"); 
+        item.classList.remove("rightColorPlace"); 
+    });
+
+   guessCode_fa_circle.forEach( (item) => {
+        item.style.color = "#242424";
+    })
+
+    randomSecretColors();
+
+    mask.classList.add("hidden");
+}
+
+function setColoring() {
+    default_color_field_fa_circle.forEach( (item, index) => {
+        item.style.color = currentColorPalette[index];
+    })
+    if(gameMode === "coop"){
+        randomSecretColors(); 
+    }else{
+        currentCodeArray = JSON.parse(sessionStorage.getItem("versusColorCode"));
+    }
+  
+}
+
+function randomSecretColors(){
+    let randomColors = currentColorPalette.sort(() => Math.random() - .5).slice(0, 4);
+    currentCodeArray = randomColors;
+}
+
+function handleColorClick(event) {
+   
+    let target = event.target.style.color;
+
+    document.querySelector(".guessContainer:nth-child(" + rowCount + ") .color_field:nth-child(" + (currentField+1) + ") > *").style.color = target;
+
+    let id = event.target.parentNode.id;
+   disableDefaultColorButton(id);  
+
+    if(colorRows.length > 2 && colorRows.length < 4){
+        colorRows[currentField] = event.target.style.color;
+        loadResults();      
+        currentField = 0;
+
+    }else{
+        colorRows[currentField] = event.target.style.color;
+        currentField = currentField + 1;
+    }
+}
+
+function loadResults() {
+    let j;
+    let t = 0;
+    let resultArray = [];
+
+    for(j = 0; j < currentCodeArray.length; j++){
+      if(colorRows.includes(currentCodeArray[j])){
+        if(currentCodeArray[j] === colorRows[j]){
+            //wenn farbe enthalten und richtige stelle, 
+            resultArray.push(2);
+        }else{
+            //wenn richtige farbe aber falsche stelle
+            resultArray.push(1);
+        }
+      }else{
+        //nicht richtige farbe und nicht richtige stelle
+        resultArray.push(0);
+      }
+    }
+
+    const sortedArray = resultArray.reverse(resultArray.sort());
+
+    for(t = 0; t < sortedArray.length; t++){
+     
+        let resultField = document.querySelector(".guessContainer:nth-child(" + rowCount + ") .result_field:nth-child(" + (t+1) + ") > *");
+        
+        if(sortedArray[t] === 2){
+            resultField.classList.add("rightColorPlace")
+        }
+
+        if(sortedArray[t] === 1){
+            resultField.classList.add("rightColor")
+        }
+    }
+
+   let checkWin = handleWin(sortedArray);
+
+    if(checkWin){
+        showSecretCode();
+        return;
+    }else{
+        if(rowCount == 1){
+            showSecretCode();
+            colorRows = [];
+            rowCount = rowCount - 1;
+            return;
+        }
+        enableDefaultColorButtons();
+    }
+    colorRows = [];
+    rowCount = rowCount - 1;
+}
+
+function handleWin(sortedArray){
+    if(sortedArray.includes(1) || sortedArray.includes(0)){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function showSecretCode(){
+    guessCode_fa_circle.forEach( (item, index) => {
+        item.style.color = currentCodeArray[index];
+    })
+
+   mask.classList.remove("hidden");
+}
+
+function disableDefaultColorButton(currentClickedButtonID){
+
+    let defaultDisableButton = document.getElementById(currentClickedButtonID);
+   
+    defaultDisableButton.children[0].classList.add("disabled");
+    defaultDisableButton.removeEventListener("click", handleColorClick);
+}
+
+function enableDefaultColorButtons(){
+   default_color_field_fa_circle.forEach(x => {
+        x.classList.remove("disabled");
+        x.addEventListener("click", handleColorClick);
+    })
+}
---
+++ script/menu.js
@@ -0,0 +1,182 @@
+let colorArray = ["rgb(53, 80, 112)", "rgb(109, 89, 122)", "rgb(181, 101, 118)", "rgb(229, 107, 111)", "rgb(232, 140, 125)", "rgb(234, 172, 139)"];
+let colorArray1 = ["rgb(18, 118, 121)", "rgb(181, 147, 146)", "rgb(53, 80, 112)", "rgb(232, 140, 125)", "rgb(69, 53, 71)", "rgb(234, 172, 139)"];
+let currentColorPalette = colorArray1;
+let colorCode = ["rgb(53, 80, 112)", "rgb(109, 89, 122)", "rgb(181, 101, 118)", "rgb(229, 107, 111)"];
+
+let categorys = document.querySelectorAll(".category");
+
+categorys.forEach(item => {
+    item.addEventListener("click", handleClickCategory);
+})
+
+
+
+let submitButton1 = document.querySelector("#submitButton1");
+let is_submit_button = true;
+
+submitButton1.addEventListener("click", onclick);
+
+function onclick(){
+    if(is_submit_button){
+        confirmClicked();
+        //is_submit_button = false;
+    }else{
+        cancleClicked();
+        //is_submit_button = true;
+    }
+    is_submit_button = !is_submit_button;
+}
+
+let inner_container1 = document.querySelector("#inner-container1");
+let container_inner_category2 = document.querySelector("#container-inner-category2");
+let currentGamemode = "Co-op";
+
+function confirmClicked(){
+    categorys.forEach(cate => {
+        if(!(cate.classList.contains("current"))){
+            cate.classList.add("notChoosenByConfirmed");
+        }
+    })
+
+    inner_container1.style.gridTemplateColumns = "1fr";
+    submitButton1.innerHTML = '<i class="fas fa-times"></i>';
+
+    if(currentGamemode === "Co-op"){
+        localStorage.setItem("defaultColorPalette", JSON.stringify(currentColorPalette));
+        sessionStorage.setItem("gameMode", JSON.stringify(currentGamemode));
+        window.location="main.html";
+    }
+
+    if(currentGamemode === "Versus"){
+        setColoring();
+        container_inner_category2.classList.add("open");
+    }
+}
+
+function cancleClicked(){
+    categorys.forEach(cate => {
+        cate.classList.remove("notChoosenByConfirmed"); 
+    })
+
+    inner_container1.style.gridTemplateColumns = "1fr 1fr";
+    submitButton1.innerHTML = '<i class="fas fa-check"></i>';
+    container_inner_category2.classList.remove("open");
+}
+
+document.querySelector("#submitButton2").addEventListener("click", onConfirmClick);
+
+function onConfirmClick(){
+    //Farbcode von icon_category2 speichern in session
+    localStorage.setItem("versusColorCode", JSON.stringify(colorCode));
+    sessionStorage.setItem("gameMode", JSON.stringify(currentGamemode));
+    window.location="main.html";
+}
+
+//click auf versusfield farbe wieder zu coloroptions
+
+
+let colorOptions = document.querySelectorAll(".colorOptions");
+
+function setColoring(){
+    colorOptions.forEach( (option, index) => {
+        option.style.color = currentColorPalette[index];
+    })
+}
+
+let currentField = 0;
+let colorRows = [];
+
+colorOptions.forEach( option => {
+    option.addEventListener("click", handleColorClick);
+})
+
+let currentColorCode = document.querySelectorAll(".versusField");
+
+currentColorCode.forEach(field => {
+    field.addEventListener("click", removeColor);
+})
+
+document.querySelectorAll(".versusField").forEach(item => {
+    console.log(item.style.color)
+})
+
+function handleColorClick(event) {
+
+    if(currentField === 4){
+        return;
+    }
+
+    let target = event.target.parentNode.style.color;
+    console.log(target)
+
+    document.querySelector("#icon_category2 > .versusField:nth-child(" + (currentField+1) + ") > *").style.color = target;
+
+    let id = event.target.parentNode.id;
+    disableDefaultColorButton(id); 
+    
+    currentField = currentField + 1;
+}
+
+
+function removeColor(event){
+ 
+    let targetColor = event.target.style.color;
+    let currentField = document.getElementById(event.target.parentNode.id);
+    colorOptions.forEach( option => {
+        if(option.children[0].classList.contains("disabled")){
+            if(targetColor === option.style.color){
+                option.children[0].classList.remove("disabled");
+                currentField.children[0].style.color = "rgb(175, 175, 175)";
+                
+                return;
+            }
+        }
+    })
+}
+
+function handleClickCategory(e) {
+    let target_ = e.target;
+
+    if(target_.nodeName === "DIV") { 
+        target_ = target_.children[0].innerText;
+    }
+
+    if(target_.nodeName === "SPAN") { 
+       target_ = target_.innerText;
+    }
+
+    if(target_ === "Co-op") {
+       currentGamemode = "Co-op";
+    }
+    
+    if(target_ === "Versus"){
+        currentGamemode = "Versus";
+    }
+
+    markCurrentCategory();
+}
+
+function markCurrentCategory() {
+    categorys.forEach( categ => {
+        categ.classList.remove("current");
+
+        if(categ.children[0].innerText === currentGamemode) {
+            categ.classList.add("current");
+        }
+    })
+}
+
+function disableDefaultColorButton(currentClickedButtonID){
+
+    let defaultDisableButton = document.getElementById(currentClickedButtonID);
+   
+    defaultDisableButton.children[0].classList.add("disabled");
+    defaultDisableButton.removeEventListener("click", handleColorClick);
+}
+
+function enableDefaultColorButtons(){
+   colorOptions.forEach(x => {
+        x.classList.remove("disabled");
+        x.addEventListener("click", handleColorClick);
+    })
+}
